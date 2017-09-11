7765d0a9b9ae6674e492f7d336f47338db6492d9
WangYe8613 <31727699+WangYe8613@users.noreply.github.com>
2017-09-11 19:38:33 +0800

    Update LinkList.cpp
    单链表表的基本操作，正序/逆序建表（尾插法/头插法）、计算单链表长度、输出单链表内容、插入、删除
    修改完善，完整代码

--- LinkList.cpp
+++ LinkList.cpp
@@ -10,7 +10,7 @@ LinkList Init_List()//初始化单链表
 {
 	
 	Node *L;
-	L = (LinkList)malloc(sizeof(Node));//强制类型转换，不懂，待问
+	L = (LinkList)malloc(sizeof(Node));//强制类型转换
 	if (L == NULL)
 	{
 		printf("初始化失败!\n");
@@ -24,6 +24,7 @@ void CreatFromHead_LinkList(LinkList L)//头插法建表（逆序）
 	Node *s;
 	char c;
 	int flag = 1;
+	cout << "请输入内容，并以“$”结束" << endl;
 	while (flag)
 	{
 		cin >> c;
@@ -46,6 +47,7 @@ void CreatFromTail_LinkList(LinkList L)//尾插法建表
 	char c;
 	int flag = 1;
 	r = L;
+	cout << "请输入内容，并以“$”结束"<<endl;
 	while (flag)
 	{
 		cin >> c;
@@ -63,7 +65,7 @@ void CreatFromTail_LinkList(LinkList L)//尾插法建表
 		}
 	}
 }
-void Length_LinkList(LinkList L)//求单链表长度
+int Length_LinkList(LinkList L)//计算单链表长度
 {
 	Node *p;
 	p = L->next;
@@ -73,7 +75,11 @@ void Length_LinkList(LinkList L)//求单链表长度
 		p = p->next;
 		Length++;
 	}
-	cout << "单链表的长度为："<<Length<<endl;
+	return Length;
+}
+void Input_Length_LinkList(LinkList L)//输出单链表长度
+{
+	cout<<"单链表的长度为：" << Length_LinkList(L)<<endl;
 }
 void Dispaly_LinkList(LinkList L)//输出单链表内容
 {
@@ -86,6 +92,84 @@ void Dispaly_LinkList(LinkList L)//输出单链表内容
 		p = p->next;
 	}
 	cout << endl;
+}
+void Get_LinkList(LinkList L)//查找指定序号对应的元素
+{
+	Node *p;
+	p = L;
+	int i;
+	flag://flag2
+	cout << "请选择序号：" << endl;
+	cin >> i;
+	if (i<1 || i>Length_LinkList(L))
+	{
+		cout << "超出范围，请重新选择"<<endl;
+		goto flag;//flag2
+	}
+	else
+	{
+		for (int j = 0; j < i; j++)
+			p = p->next;
+	}
+	cout<< "第"<<i<<"元素是："<<p->data << endl;
+}
+void Insert_LinkList(LinkList L)//给指定位置插入元素
+{
+	Node *p,*q;
+	p = L;
+	q = Init_List();//初始化q结点
+	char e;
+	int i;
+	flag://flag3
+	cout << "请选择插入的位置"<< endl;
+	cin >> i;
+	if (i<1 || i>Length_LinkList(L)+1)//判断插入的位置是否合理
+	{
+		cout << "超出范围，请重新选择" << endl;
+		goto flag;//flag3
+	}
+	else if (i == Length_LinkList(L)+1)//插在表尾
+	{
+		cout << "请输入插入的元素："<<endl;
+		cin >> e;
+		for (int j = 0; j < i-1; j++)
+			p = p->next;
+		q->data = e;
+		p->next = q;
+	}
+	else//插在表中
+	{
+		cout << "请输入插入的元素：" << endl;
+		cin >> e;
+		for (int j = 0; j < i-1; j++)
+			p = p->next;
+		q->data = e;
+		q->next = p->next;
+		p->next = q;
+	}
+}
+void Delete_LinkList(LinkList L)//删除序号位置的元素
+{
+	Node *p,*q;
+	p = L;
+	int i;
+	flag://flag4
+	cout << "请选择要删除的元素序号"<<endl;
+	cin >> i;
+	if (i<1||i>Length_LinkList(L))
+	{
+		cout << "超出单链表范围，请重新输入"<<endl;
+		goto flag;//flag4
+	}
+	else
+	{
+		for (int j = 0; j < i-1; j++)
+			p = p->next;
+		q = p->next;
+		p->next = q->next;
+		int e = q->data;
+		free(q);
+	}
 }
 int main()
 {
@@ -100,6 +184,7 @@ int main()
 		<< endl << "4.输出单链表的内容"
 		<< endl << "5.查找指定序号的元素"
 		<< endl << "6.在指定位置插入元素"
+		<< endl << "7.删除指定序号的元素"
 		<< endl << "0.退出程序" << endl;
 	int n;
 	cin >> n;
@@ -107,12 +192,16 @@ int main()
 	{
 	case 1:CreatFromTail_LinkList(L); break;
 	case 2:CreatFromHead_LinkList(L); break;
-	case 3:Length_LinkList(L); break;
+	case 3:Input_Length_LinkList(L); break;
 	case 4:Dispaly_LinkList(L); break;
-	default:cout << "谢谢使用！";
-		break;
+	case 5:Get_LinkList(L); break;
+	case 6:Insert_LinkList(L); break;
+	case 7:Delete_LinkList(L); break;
+	default:cout << "谢谢使用！"<<endl; 
+		    system("pause");
+		return 0;
 	}
-	cout << "是否继续使用顺序表其他功能？1 for 是；2 for 否" << endl;
+	cout << "是否继续使用顺序表其他功能？"<<endl<<"1 for 是；2 for 否" << endl;
 	int ss;
 	cin >> ss;
 	if (ss == 1)
@@ -121,4 +210,5 @@ int main()
 	}
 	system("pause");
 	return 0;
+
 }
